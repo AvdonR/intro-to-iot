e2428095c604307ff5760dde52a19cd6b231ec3e
Avdon Racki <avdon@racki.ca>
2020-11-30 16:07:09 -0500

    Initial server setup
    Express server with Okta user authentication. Uses TypeScript with views and partials

---
+++ src/routes/index.ts
@@ -0,0 +1,28 @@
+import * as express from "express";
+
+export const register = ( app: express.Application ) => {
+    const oidc = app.locals.oidc;
+
+    // define a route handler for the default home page
+    app.get( "/", ( req: any, res ) => {
+        const user = req.userContext ? req.userContext.userinfo : null;
+        res.render( "index", { isAuthenticated: req.isAuthenticated(), user } );
+    } );
+
+    // define a secure route handler for the login page that redirects to /locks
+    app.get( "/login", oidc.ensureAuthenticated(), ( req, res ) => {
+        res.redirect( "/locks" );
+    } );
+
+    // define a route to handle logout
+    app.get( "/logout", ( req: any, res ) => {
+        req.logout();
+        res.redirect( "/" );
+    } );
+
+    // define a secure route handler for the locks page
+    app.get( "/locks", oidc.ensureAuthenticated(), ( req: any, res ) => {
+        const user = req.userContext ? req.userContext.userinfo : null;
+        res.render( "locks", { isAuthenticated: req.isAuthenticated(), user } );
+    } );
+};
